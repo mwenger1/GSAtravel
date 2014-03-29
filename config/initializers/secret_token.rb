0bb3675a1587a47309d2ec12e198ec3167f88708
Michael Wenger <michaelwenger27@gmail.com>
2014-03-29 09:45:16 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GSAtravel::Application.config.secret_key_base = 'db4cff727f83e3ab6d6f130a23c782e4e082cc91d09e45d467838148b6a52c17d592007fbefa101187fce022ab82e098e8704fee5f573d794ff13e65bf763fd6'
