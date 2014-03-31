c1267b5ac712a6dc3620cc1982cd026f44be2086
Michael Wenger <michaelwenger27@gmail.com>
2014-03-31 12:25:41 -0400

    Filtered out bad flight data

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -8,36 +8,40 @@ class ImportController < ApplicationController
 
   def run
     require 'csv'
-    csv = File.read("#{Rails.public_path}/flight_reservation1.csv")
+    csv = File.read("#{Rails.public_path}/flight_reservation1_small.csv")
     encoded_csv = csv.encode!('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
     parsed_csv = CSV.parse(encoded_csv)
 
     counter = 0
     parsed_csv.each do |entry|
         if counter != 0
-            f = FlightReservation.new
-            f.gds_record_locator = entry[0]
-            f.reservation_date = Date.strptime(entry[1], '%m/%d/%y') unless entry[1].nil?
-            f.ticket_issue_date = Date.strptime(entry[2], '%m/%d/%y') unless entry[2].nil?
-            f.trip_departure_date = Date.strptime(entry[3], '%m/%d/%y') unless entry[3].nil?
-            f.ticket_number = entry[4]
-            f.airline_name = entry[5]
-            f.cabin_class = entry[6]
-            f.domestic_international_indicator = entry[7]
-            f.origin_airport_code = entry[8]
-            f.destination_airport_code = entry[9]
-            f.routing = entry[10]
-            f.invoice_amount = entry[11].to_f
-            f.tax_amount = entry[12].to_f
-            f.total_amount = entry[13].to_f
-            f.mileage = entry[14].to_f
-            f.exchange_indicator = entry[15]
-            f.exchange_original_ticket_number = entry[16]
-            f.refund_indicator = entry[17]
-            f.original_invoice_number = entry[18]
-            f.online_indicator = entry[19]
-            f.fare_category = entry[20]
-            f.save
+
+            ## FILTERS OUT RETURNS AND REFUNDS @TODO incorporate refunds
+            unless (entry[17] == "Y" || entry[17] == "P" || entry[15] == "Y" || entry[13].blank? || entry[4].blank?)
+                f = FlightReservation.new
+                f.gds_record_locator = entry[0]
+                f.reservation_date = Date.strptime(entry[1], '%m/%d/%y') unless entry[1].nil?
+                f.ticket_issue_date = Date.strptime(entry[2], '%m/%d/%y') unless entry[2].nil?
+                f.trip_departure_date = Date.strptime(entry[3], '%m/%d/%y') unless entry[3].nil?
+                f.ticket_number = entry[4]
+                f.airline_name = entry[5]
+                f.cabin_class = entry[6]
+                f.domestic_international_indicator = entry[7]
+                f.origin_airport_code = entry[8]
+                f.destination_airport_code = entry[9]
+                f.routing = entry[10]
+                f.invoice_amount = entry[11].to_f
+                f.tax_amount = entry[12].to_f
+                f.total_amount = entry[13].to_f
+                f.mileage = entry[14].to_f
+                f.exchange_indicator = entry[15]
+                f.exchange_original_ticket_number = entry[16]
+                f.refund_indicator = entry[17]
+                f.original_invoice_number = entry[18]
+                f.online_indicator = entry[19]
+                f.fare_category = entry[20]
+                f.save
+           end
         end
         counter = counter + 1
     end
