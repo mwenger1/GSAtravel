af3bb5d410b8d387f0c4104031091da2b203a6a8
Michael Wenger <michaelwenger27@gmail.com>
2014-03-31 15:55:42 -0400

    Updated helper methods to specify integer to fix heroku bug

--- app/models/flight_reservation.rb
+++ app/models/flight_reservation.rb
@@ -2,13 +2,13 @@ class FlightReservation < ActiveRecord::Base
     before_save :days_in_advance, :calculate_benchmark_percentage, :calculate_benchmark_difference
 
     def days_in_advance
-        self.days_in_advance = self.trip_departure_date - self.reservation_date
+        self.days_in_advance = (self.trip_departure_date - self.reservation_date).to_i
     end
     def calculate_benchmark_percentage
-        self.benchmark_rate_percentage = (((self.benchmark_rate* 100)/ self.total_amount)-100).round(2)
+        self.benchmark_rate_percentage = (((self.benchmark_rate* 100)/ self.total_amount)-100).to_f.round(2)
     end
 
     def calculate_benchmark_difference
-        self.benchmark_rate_difference = self.benchmark_rate - self.total_amount
+        self.benchmark_rate_difference = (self.benchmark_rate - self.total_amount).to_f
     end
 end
