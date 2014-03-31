0c77e38d9301044b3e00df6c3bcdda9522670e8c
Michael Wenger <michaelwenger27@gmail.com>
2014-03-30 20:31:17 -0400

    Added hotel reservation model

---
+++ db/migrate/20140331003059_create_hotel_reservations.rb
@@ -0,0 +1,24 @@
+class CreateHotelReservations < ActiveRecord::Migration
+  def change
+    create_table :hotel_reservations do |t|
+      t.string :GDS_Record_Locator
+      t.date :Reservation_Date
+      t.date :Check_In_Date
+      t.date :Check_Out_Date
+      t.string :Hotel_Chain_Code
+      t.string :Hotel_Chain_Name
+      t.string :Hotel_Property_Name
+      t.string :Hotel_Address
+      t.string :Hotel_City_Name
+      t.string :Hotel_State
+      t.string :Hotel_Zip_Code
+      t.string :Hotel_Country_Code
+      t.integer :Number_of_Room_Nights
+      t.float :Daily_Rate
+      t.string :Room_Rate_Code
+      t.bool :Online_Indicator
+
+      t.timestamps
+    end
+  end
+end
