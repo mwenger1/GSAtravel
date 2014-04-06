27776e1ad871358a159f985b45abc50555b5cc24
Michael Wenger <michaelwenger27@gmail.com>
2014-04-06 15:33:39 -0400

    Added car import data

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -2,6 +2,7 @@ class ImportController < ApplicationController
   def index
     @flight_transactions = FlightReservation.all.count
     @hotel_transactions = HotelReservation.all.count
+    @car_rental_transactions = CarRentalReservation.all.count
 
   end
 
@@ -72,35 +73,28 @@ class ImportController < ApplicationController
 
             ## FILTERS OUT RETURNS AND REFUNDS @TODO incorporate refunds
             unless (entry[17] == "Y" || entry[17] == "P" || entry[15] == "Y" || entry[13].blank? || entry[4].blank? || entry[3].blank?)
-                f = FlightReservation.new
-                f.gds_record_locator = entry[0]
-                f.reservation_date = Date.strptime(entry[1], '%m/%d/%y') unless entry[1].nil?
-                f.ticket_issue_date = Date.strptime(entry[2], '%m/%d/%y') unless entry[2].nil?
-                f.trip_departure_date = Date.strptime(entry[3], '%m/%d/%y') unless entry[3].nil?
-                f.ticket_number = entry[4]
-                f.airline_name = entry[5]
-                f.cabin_class = entry[6]
-                f.domestic_international_indicator = entry[7]
-                f.origin_airport_code = entry[8]
-                f.destination_airport_code = entry[9]
-                f.routing = entry[10]
-                f.invoice_amount = entry[11].to_f
-                f.tax_amount = entry[12].to_f
-                f.total_amount = entry[13].to_f
-                f.benchmark_rate = calculate_benchmark_rate(entry[13])
-                f.mileage = entry[14].to_f.round(1)
-                f.exchange_indicator = entry[15]
-                f.exchange_original_ticket_number = entry[16]
-                f.refund_indicator = entry[17]
-                f.original_invoice_number = entry[18]
-                f.online_indicator = entry[19]
-                f.fare_category = entry[20]
-                f.save
+                c = CarRentalReservation.new
+                c.gds_record_locator = entry[0]
+                c.reservation_date = Date.strptime(entry[1], '%m/%d/%y') unless entry[1].nil?
+                c.car_pick_up_date = Date.strptime(entry[2], '%m/%d/%y') unless entry[2].nil?
+                c.car_drop_off_date = Date.strptime(entry[3], '%m/%d/%y') unless entry[3].nil?
+                c.car_chain_code = entry[4]
+                c.car_chain_name = entry[5]
+                c.car_pick_up_city = entry[6]
+                c.car_pick_up_state = entry[7]
+                c.car_drop_off_city = entry[8]
+                c.car_drop_off_state = entry[9]
+                c.car_rental_days = entry[10].to_i
+                c.car_daily_rate = entry[11].to_f
+                c.car_category = entry[12]
+                c.online_indicator = entry[13]
+                c.save
            end
         end
         counter = counter + 1
     end
-    redirect_to import_index_path, notice: "File successfully imported"
+
+    redirect_to import_index_path, notice: "Car file successfully imported"
   end
 
 
@@ -153,7 +147,7 @@ class ImportController < ApplicationController
         end
         counter = counter + 1
     end
-    redirect_to import_index_path, notice: "File successfully imported"
+    redirect_to import_index_path, notice: "Airline file successfully imported"
   end
 
   def delete_all
