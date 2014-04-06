47494ac9c2eb684fdcb5936c498481547411d8b1
Michael Wenger <michaelwenger27@gmail.com>
2014-04-06 15:10:07 -0400

    Added hotel import logic

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -1,6 +1,8 @@
 class ImportController < ApplicationController
   def index
-    @flight_reservations = FlightReservation.all.count
+    @flight_transactions = FlightReservation.all.count
+    @hotel_transactions = HotelReservation.all.count
+
   end
 
   def show
@@ -25,36 +27,29 @@ class ImportController < ApplicationController
         if counter != 0
 
             ## FILTERS OUT RETURNS AND REFUNDS @TODO incorporate refunds
-            unless (entry[17] == "Y" || entry[17] == "P" || entry[15] == "Y" || entry[13].blank? || entry[4].blank? || entry[3].blank?)
-                f = FlightReservation.new
+            unless (false) ## ADD FILTERS
+                f = HotelReservation.new
                 f.gds_record_locator = entry[0]
                 f.reservation_date = Date.strptime(entry[1], '%m/%d/%y') unless entry[1].nil?
-                f.ticket_issue_date = Date.strptime(entry[2], '%m/%d/%y') unless entry[2].nil?
-                f.trip_departure_date = Date.strptime(entry[3], '%m/%d/%y') unless entry[3].nil?
-                f.ticket_number = entry[4]
-                f.airline_name = entry[5]
-                f.cabin_class = entry[6]
-                f.domestic_international_indicator = entry[7]
-                f.origin_airport_code = entry[8]
-                f.destination_airport_code = entry[9]
-                f.routing = entry[10]
-                f.invoice_amount = entry[11].to_f
-                f.tax_amount = entry[12].to_f
-                f.total_amount = entry[13].to_f
-                f.benchmark_rate = calculate_benchmark_rate(entry[13])
-                f.mileage = entry[14].to_f.round(1)
-                f.exchange_indicator = entry[15]
-                f.exchange_original_ticket_number = entry[16]
-                f.refund_indicator = entry[17]
-                f.original_invoice_number = entry[18]
-                f.online_indicator = entry[19]
-                f.fare_category = entry[20]
+                f.check_in_date = Date.strptime(entry[2], '%m/%d/%y') unless entry[2].nil?
+                f.check_out_date = Date.strptime(entry[3], '%m/%d/%y') unless entry[3].nil?
+                f.hotel_chain_code = entry[4]
+                f.hotel_chain_name = entry[5]
+                f.hotel_property_name = entry[6]
+                f.hotel_address = entry[7]
+                f.hotel_city_name = entry[8]
+                f.hotel_state = entry[9]
+                f.hotel_zip_code = entry[10]
+                f.hotel_country_code = entry[11]
+                f.number_of_room_nights = entry[12].to_i
+                f.daily_rate = entry[13].to_f
+                f.room_rate_code = calculate_benchmark_rate(entry[13])
                 f.save
            end
         end
         counter = counter + 1
     end
-    redirect_to import_index_path, notice: "File successfully imported"
+    redirect_to import_index_path, notice: "Hotel file successfully imported"
   end
 
   def run_car_rental
