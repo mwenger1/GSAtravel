6983087bcdcfaceb49fa3907cfb8d4f6e6ba5cd0
Michael Wenger <michaelwenger27@gmail.com>
2014-04-06 20:48:55 -0400

    Added days_in_advance to importer for hotels and car rentals

--- app/models/car_rental_reservation.rb
+++ app/models/car_rental_reservation.rb
@@ -1,2 +1,6 @@
 class CarRentalReservation < ActiveRecord::Base
+    before_save :days_in_advance
+    def days_in_advance
+        self.days_in_advance = (self.car_pick_up_date - self.reservation_date).to_i
+    end
 end
--- app/models/hotel_reservation.rb
+++ app/models/hotel_reservation.rb
@@ -1,6 +1,6 @@
 class HotelReservation < ActiveRecord::Base
- before_save :days_in_advance, :calculate_benchmark_percentage, :calculate_benchmark_difference
+    before_save :days_in_advance
     def days_in_advance
-        self.days_in_advance = (self.car_pick_up_date - self.reservation_date).to_i
+        self.days_in_advance = (self.check_in_date - self.reservation_date).to_i
     end
 end
