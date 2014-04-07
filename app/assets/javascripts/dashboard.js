28a5f99ff88176fde25150b1c1b46a00eab1af45
Michael Wenger <michaelwenger27@gmail.com>
2014-04-07 19:53:28 -0400

    Finished popops

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -396,6 +396,41 @@ ready = function() {
         $("#tablesWrapper .dataTables_wrapper").hide();
         $(tmpName).show();
     });
+
+    var isVisible = false;
+
+    var hideAllPopovers = function() {
+       $('.popup-marker').each(function() {
+            $(this).popover('hide');
+        });
+    };
+
+    $('.popup-marker').popover({
+        html: true,
+        trigger: 'manual'
+    }).on('click', function(e) {
+        // if any other popovers are visible, hide them
+        if(isVisible) {
+            hideAllPopovers();
+        }
+
+        $(this).popover('show');
+
+        // handle clicking on the popover itself
+        $('.popover').off('click').on('click', function(e) {
+            e.stopPropagation(); // prevent event for bubbling up => will not get caught with document.onclick
+        });
+
+        isVisible = true;
+        e.stopPropagation();
+    });
+
+
+    $(document).on('click', function(e) {
+        hideAllPopovers();
+        isVisible = false;
+    });
+
 };
 
 $(document).ready(ready);
