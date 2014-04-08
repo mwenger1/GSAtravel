ae4f18e9219a9c5edc1e7baf8c86e2553d03cb61
Michael Wenger <michaelwenger27@gmail.com>
2014-04-07 20:26:18 -0400

    Got chart to laod succesffully

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -69,7 +69,21 @@ require 'gchart'
     @vendor_hotel_pie = Gchart.pie(:data => [20, 35, 45])
     @vendor_car_rental_pie = Gchart.pie(:data => [40, 35, 25])
 
+    data_table = GoogleVisualr::DataTable.new
+    # Add Column Headers
+    data_table.new_column('string', 'Year' )
+    data_table.new_column('number', 'Sales')
+    data_table.new_column('number', 'Expenses')
 
+    # Add Rows and Values
+    data_table.add_rows([
+        ['2004', 1000, 400],
+        ['2005', 1170, 460],
+        ['2006', 660, 1120],
+        ['2007', 1030, 540]
+    ])
+    option = { width: 400, height: 240, title: 'Company Performance' }
+    @chart = GoogleVisualr::Interactive::AreaChart.new(data_table, option)
   end
 
   def ways_to_save
