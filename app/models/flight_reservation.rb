3245a5fa8b15ad6de594893ed55838c4bc06cdb2
Michael Wenger <michaelwenger27@gmail.com>
2014-04-07 23:49:46 -0400

    set up cost per mile in database

--- app/models/flight_reservation.rb
+++ app/models/flight_reservation.rb
@@ -1,5 +1,5 @@
 class FlightReservation < ActiveRecord::Base
-    before_save :days_in_advance, :calculate_benchmark_percentage, :calculate_benchmark_difference
+    before_save :days_in_advance, :calculate_benchmark_percentage, :calculate_benchmark_difference, :calculate_cost_per_mile
 
     def days_in_advance
         self.days_in_advance = (self.trip_departure_date - self.reservation_date).to_i
@@ -10,6 +10,10 @@ class FlightReservation < ActiveRecord::Base
 
     def calculate_benchmark_difference
         self.benchmark_rate_difference = (self.benchmark_rate - self.total_amount).to_f
+    end
+
+    def calculate_cost_per_mile
+        self.cost_per_mile = (self.total_amount / self.mileage).to_f.round(2)
     end
 
     def shorten_fare_category
