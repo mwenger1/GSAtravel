e6cee82ecfa20e8319c66a206e7cbe9b01884bbb
Michael Wenger <michaelwenger27@gmail.com>
2014-04-09 08:20:49 -0400

    Updated ng-show to work properly

--- app/assets/javascripts/partials/fat.js
+++ app/assets/javascripts/partials/fat.js
@@ -5,6 +5,15 @@ angular.module('google-chart-sample').controller("FatChartCtrl",["$scope", funct
     $scope.report_type = "vendor";
     $scope.chart_number = 1;
 
+  $scope.count = 0;
+    $scope.updateChartNumber = function(number){
+        $scope.chart_number = number;
+      };
+
+    $scope.updateChartOnly = function(type){
+        $scope.report_type = type;
+      };
+
 }]);
 
 
