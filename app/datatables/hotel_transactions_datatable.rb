c54441c66351de9e54f751e5b37fcc45cc7dbf57
Michael Wenger <michaelwenger27@gmail.com>
2014-04-08 22:16:18 -0400

    Updated header to rate

--- app/datatables/hotel_transactions_datatable.rb
+++ app/datatables/hotel_transactions_datatable.rb
@@ -24,11 +24,11 @@ private
         hotel.gds_record_locator,
         hotel.reservation_date.strftime('%b %d, %Y'),
         hotel.days_in_advance.to_s + " days",
+        hotel.hotel_city_name,
         hotel.hotel_chain_name.titleize,
         hotel.online_indicator,
         number_to_currency(hotel.daily_rate),
         number_to_currency(hotel.total_amount),
-        hotel.hotel_city_name,
         hotel.hotel_chain_name,
         hotel.hotel_chain_name,
         hotel.hotel_chain_name,
@@ -65,7 +65,7 @@ private
   end
 
   def sort_column
-    columns = %w[gds_record_locator reservation_date days_in_advance hotel_chain_name online_indicator daily_rate daily_rate total_amount ]
+    columns = %w[gds_record_locator reservation_date days_in_advance hotel_city_name hotel_chain_name online_indicator daily_rate daily_rate total_amount ]
     columns[params[:iSortCol_0].to_i]
   end
 
