22829b2d70e270b7d0d85feac0cbc9bfc45e0401
Michael Wenger <michaelwenger27@gmail.com>
2014-04-09 22:15:58 -0400

    Updated data being sent to the car transactions table

--- app/datatables/car_rental_transactions_datatable.rb
+++ app/datatables/car_rental_transactions_datatable.rb
@@ -32,10 +32,11 @@ private
         rental.car_rental_days,
         number_to_currency(rental.total_amount),
         number_to_currency(rental.total_amount),
-        number_to_currency(rental.total_amount),
-        rental.gds_record_locator,
-        rental.gds_record_locator,
-        rental.gds_record_locator
+        rental.reservation_date,
+        rental.car_drop_off_date,
+        rental.car_chain_code,
+        rental.car_drop_off_city,
+        rental.car_drop_off_state
       ]
     end
   end
