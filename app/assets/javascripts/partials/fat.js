3e35dffe2f92ba299c11a22f7e834a785284720d
Michael Wenger <michaelwenger27@gmail.com>
2014-04-09 21:10:45 -0400

    Set up 3 charts for hannah to work on

--- app/assets/javascripts/partials/fat.js
+++ app/assets/javascripts/partials/fat.js
@@ -1,13 +1,21 @@
 'use strict';
 
-angular.module('google-chart-sample').controller("FatChartCtrl",["$scope", function ($scope) {
+angular.module('google-chart-sample').controller("FatChartCtrl",["$scope","$http", function ($scope, $http) {
+
+    $http.get('/dashboard/flight_vendor_data.json').success(function(data) {
+        $scope.items = data;
+    }).error(function(error) {
+        console.log('The file did not load');
+    });
+
     $scope.report_type = "vendor";
     $scope.chart_number = 1;
 
-    var chart1 = {};
-    chart1.type = "PieChart";
-    chart1.displayed = false;
-    chart1.data = {"cols": [
+
+    var vendorChart1 = {};
+    vendorChart1.type = "PieChart";
+    vendorChart1.displayed = false;
+    vendorChart1.data = {"cols": [
         {id: "month", label: "Month", type: "string"},
         {id: "laptop-id", label: "Laptop", type: "number"},
         {id: "desktop-id", label: "Desktop", type: "number"},
@@ -38,7 +46,7 @@ angular.module('google-chart-sample').controller("FatChartCtrl",["$scope", funct
         ]}
     ]};
 
-    chart1.options = {
+    vendorChart1.options = {
         "title": "Sales per month",
         "isStacked": "true",
         "fill": 20,
@@ -52,10 +60,10 @@ angular.module('google-chart-sample').controller("FatChartCtrl",["$scope", funct
     };
 
 
-    var chart2 = {};
-    chart2.type = "ColumnChart";
-    chart2.displayed = false;
-    chart2.data = {"cols": [
+    var vendorChart2 = {};
+    vendorChart2.type = "ColumnChart";
+    vendorChart2.displayed = false;
+    vendorChart2.data = {"cols": [
         {id: "month", label: "Month", type: "string"},
         {id: "laptop-id", label: "Laptop", type: "number"},
         {id: "desktop-id", label: "Desktop", type: "number"},
@@ -86,7 +94,7 @@ angular.module('google-chart-sample').controller("FatChartCtrl",["$scope", funct
         ]}
     ]};
 
-    chart2.options = {
+    vendorChart2.options = {
         "title": "Sales per month",
         "isStacked": "true",
         "fill": 20,
@@ -100,15 +108,75 @@ angular.module('google-chart-sample').controller("FatChartCtrl",["$scope", funct
     };
 
 
+   var vendorChart3 = {};
+    vendorChart3.type = "BarChart";
+    vendorChart3.displayed = false;
+    vendorChart3.data = {"cols": [
+        {id: "month", label: "Month", type: "string"},
+        {id: "laptop-id", label: "Laptop", type: "number"},
+        {id: "desktop-id", label: "Desktop", type: "number"},
+        {id: "server-id", label: "Server", type: "number"},
+        {id: "cost-id", label: "Shipping", type: "number"}
+    ], "rows": [
+        {c: [
+            {v: "January"},
+            {v: 19, f: "42 items"},
+            {v: 12, f: "Ony 12 items"},
+            {v: 7, f: "7 servers"},
+            {v: 4}
+        ]},
+        {c: [
+            {v: "February"},
+            {v: 13},
+            {v: 1, f: "1 unit (Out of stock this month)"},
+            {v: 12},
+            {v: 2}
+        ]},
+        {c: [
+            {v: "March"},
+            {v: 24},
+            {v: 5},
+            {v: 11},
+            {v: 6}
 
-    $scope.chart = chart1;
-    $scope.chart2 = chart2;
+        ]}
+    ]};
+
+    vendorChart3.options = {
+        "title": "Sales per month",
+        "isStacked": "true",
+        "fill": 20,
+        "displayExactValues": true,
+        "vAxis": {
+            "title": "Sales unit", "gridlines": {"count": 10}
+        },
+        "hAxis": {
+            "title": "Date"
+        }
+    };
+
+
+    $scope.vendorChart1 = vendorChart1;
+    $scope.vendorChart2 = vendorChart2;
+    $scope.vendorChart3 = vendorChart3;
+
+    // <option value="AreaChart">AreaChart</option>
+    // <option value="PieChart">PieChart</option>
+    // <option value="ColumnChart">ColumnChart</option>
+    // <option value="LineChart">LineChart</option>
+    // <option value="Table">Table</option>
+    // <option value="BarChart">BarChart</option>
+
+
     $scope.cssStyle = "height:600px; width:100%;";
 
     $scope.reportTypeChange = function (report) {
         $scope.report_type = report;
     }
 
+    $scope.updateChartNumber = function (chartNumber) {
+        $scope.chart_number = chartNumber;
+    }
 
 }]);
 
